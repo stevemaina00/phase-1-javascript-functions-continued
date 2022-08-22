4c8e474b6cddf78a77a0a9246cd2df68edadcd9c
Steve <steve.maina@student.moringaschool.com>
2022-08-22 21:37:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(emphatic="a dedicated programmer") {
+    return `You are ${style}${emphatic}${style}!`
+  }
+}
+    
+   
+    
